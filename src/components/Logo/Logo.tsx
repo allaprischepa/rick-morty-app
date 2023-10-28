7111252642f0872fba591e25845a44a6248277a0
Alla Prishchepa <tyurina.alla@gmail.com>
2023-10-28 22:08:24 +0400

    fix: fix logo path

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,11 +1,12 @@
 import { Component } from 'react';
 import './Logo.scss';
+import logo from '../../../public/images/rick-and-morty-logo-250x76.png';
 
 class Logo extends Component {
   render() {
     return (
       <div className="logo">
-        <img src="../../public/images/rick-and-morty-logo-250x76.png"></img>
+        <img src={logo}></img>
       </div>
     );
   }
