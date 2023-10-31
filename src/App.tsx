4404ccf74aae15dcd5aa8318dc6895d307cdea7d
Alla Prishchepa <tyurina.alla@gmail.com>
2023-10-31 17:44:50 +0400

    refactor: migrate app to a function

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,29 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import './App.scss';
 import CharactersList from './components/CharactersList/CharactersList';
 import SearchBar from './components/SearchBar/SearchBar';
 import Logo from './components/Logo/Logo';
 import ErrorButton from './components/ErrorButton/ErrorButton';
 import ErrorBoundary from './components/ErrorBoundary/ErrorBoundary';
-import { IProps } from './types/types';
 
-const SEARCH_TERM_NAME = 'RMAppSearchTerm';
-const savedTerm = localStorage.getItem(SEARCH_TERM_NAME);
+function App() {
+  const SEARCH_TERM_NAME = 'RMAppSearchTerm';
+  const savedTerm = localStorage.getItem(SEARCH_TERM_NAME);
+  const [searchTerm, setSearchTerm] = useState(savedTerm ? savedTerm : '');
 
-interface State {
-  searchTerm: string;
-}
-
-class App extends Component<IProps, State> {
-  state: State = {
-    searchTerm: savedTerm ? savedTerm : '',
+  const updateSearchTerm = (value: string) => {
+    setSearchTerm(value);
+    localStorage.setItem(SEARCH_TERM_NAME, value);
   };
 
-  updateSearchTerm = (value: string) => {
-    const newValue = value.trim();
-
-    if (this.state.searchTerm !== newValue) {
-      this.setState({ searchTerm: newValue });
-      localStorage.setItem(SEARCH_TERM_NAME, newValue);
-    }
-  };
-
-  render() {
-    return (
-      <>
-        <ErrorBoundary>
-          <ErrorButton></ErrorButton>
-          <Logo></Logo>
-          <SearchBar
-            searchTerm={this.state.searchTerm}
-            updateSearchTerm={this.updateSearchTerm}
-          ></SearchBar>
-          <CharactersList searchTerm={this.state.searchTerm}></CharactersList>
-        </ErrorBoundary>
-      </>
-    );
-  }
+  return (
+    <ErrorBoundary>
+      <ErrorButton />
+      <Logo />
+      <SearchBar searchTerm={searchTerm} updateSearchTerm={updateSearchTerm} />
+      <CharactersList searchTerm={searchTerm} />
+    </ErrorBoundary>
+  );
 }
 
 export default App;
