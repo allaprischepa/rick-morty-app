8e6012c6bab942be8cc41412110146086f7e994b
Alla Prishchepa <tyurina.alla@gmail.com>
2023-11-24 23:32:58 +0400

    fix: fix Details page

--- pages/page/[pageID]/details/[characterID].tsx
+++ pages/page/[pageID]/details/[characterID].tsx
@@ -2,8 +2,8 @@ import Content from '../../../../src/components/Content/Content';
 import { wrapper } from '../../../../src/state/store';
 import { serverSidePropsCallback } from '../../../../src/utils/utils';
 
-function DetailsPage() {
-  return <Content />;
+function DetailsPage(props) {
+  return <Content props={props} />;
 }
 
 export default DetailsPage;
